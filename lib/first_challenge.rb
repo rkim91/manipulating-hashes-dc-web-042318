fe904165d599a9789020f7ec0d9f022202e0ab36
Richard Kim <richeekim@gmail.com>
2018-04-04 00:04:05 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -16,7 +16,7 @@ def first_challenge
   contacts.each do |person, data|
     data.each do |attribute, value|
       if attribute == :favorite_icecream_flavors
-        favorite_icecream_flavors.delete_if {|x| x == "strawberry"}
+        :favorite_icecream_flavors.delete_if {|x| x == "strawberry"}
       end
     end
   end
